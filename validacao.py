9d15d8d32083f689d655f632dc903a191dfa6dae
Marco-Costa1602 <marco.costa1602@gmail.com>
2021-05-08 00:16:16 -0300

    Progresso - GDFU
    Changelog:
    
    Main:
    -Função query agora busca dicionários
    -Nova função para SELECT com LIKE
    
    Serializers:
    -Dados da DB agr são recebidos pelos nomes das chaves de uma variável.
    
    Gêneros:
    - Alterada a função de GET
    - Alterada a função de POST
    - Alterada a função de PATCH
    - Alterada a função de DELETE
    
    Diretores:
    - Alterada a função de GET
    - Alterada a função de POST
    - Alterada a função de PATCH
    - Alterada a função de DELETE
    
    Filmes:
    - Criada a validação para filmes
    - Criada a serialização para filmes
    - Criado novos models para filmes
    - Adicionada a função de GET
    - Adicionada a função de POST
    - Adicionada a função de PATCH
    - Adicionada a função de DELETE
    
    Usuários:
    - Criada a validação para usuários
    - Criado novos models para usuários
    - Adicionada a função de GET
    - Adicionada a função de POST
    - Adicionada a função de PATCH
    - Adicionada a função de DELETE
    
    Tenho que ver como devo começar as locações, mas ainda não tenho ideia. Qualquer coisa atualizarei aqui, mas estou aceitando pontapés iniciais.

--- validacao.py
+++ validacao.py
@@ -3,7 +3,7 @@ from main import select, query
 
 
 def valida_genero(nome):
-    list_holder = [item[0] for item in query("SELECT nome FROM generos")]
+    list_holder = [item['nome'] for item in query("SELECT nome FROM generos")]
     if nome in list_holder:
         return False
     if len(nome) == 0:
@@ -13,12 +13,45 @@ def valida_genero(nome):
     return True
 
 
-def valida_diretor(nome):
-    list_holder = [item[0] for item in query("SELECT nome_completo FROM diretores")]
-    if nome in list_holder:
+def valida_diretor(nome_completo):
+    list_holder = [item['nome_completo'] for item in query("SELECT nome_completo FROM diretores")]
+    if nome_completo in list_holder:
         return False
-    if len(nome) == 0:
+    if len(nome_completo) == 0:
         return False
-    elif len(nome) > 255:
+    elif len(nome_completo) > 255:
         return False
+    return True
+
+
+def valida_filme(titulo, ano, classificacao, preco, diretores_id, generos_id):
+    diretor_id_holder = [item['id'] for item in query("SELECT id FROM diretores")]
+    genero_id_holder = [item['id'] for item in query("SELECT id FROM generos")]
+    title_holder = [item['titulo'] for item in query("SELECT titulo FROM filmes")]
+
+    if len(titulo) == 0 or titulo in title_holder or len(titulo) > 255:
+        return False
+
+    elif ano < 1950 or ano > 2021:
+        return False
+
+    elif preco <= 0:
+        return False
+
+    elif diretores_id not in diretor_id_holder:
+        return False
+
+    elif generos_id not in genero_id_holder:
+        return False
+
+    return True
+
+
+def valida_usuario(nome_completo, CPF):
+    if len(nome_completo) == 0:
+        return False
+
+    if len(CPF) != 14:
+        return False
+
     return True

ff4653ea10e169fb80637782437b3322af07d7b4
Marco-Costa1602 <marco.costa1602@gmail.com>
2021-05-08 00:40:25 -0300

    Progresso GDFU [Again]
    TInha dado alguns erros com o pull passado, por isso, estou fazendo novamente.

--- app.py
+++ app.py
@@ -1,124 +1,174 @@
 from flask import Flask, jsonify, request
-from main import query, execute
-from serializer import genero_name_web, genero_name_db
-from validacao import valida_genero
-from models import insert_genero, get_genero, delete_genero, alter_genero
-
-
-#Serializer serve para validação e formatação
+from serializer import genero_name_web, genero_name_db, diretor_name_web, diretor_name_db, nome_diretor_from_web, titulo_filme_from_web, filme_name_web, filme_name_db, usuario_from_web, usuario_from_db, nome_usuario_from_web, nome_genero_from_web
+from validacao import valida_genero, valida_diretor, valida_filme, valida_usuario
+from models import insert_genero, get_genero, select_genero, delete_genero, update_genero, get_diretor, select_diretor, insert_diretor, delete_diretor, update_diretor, select_filme, delete_fime, update_filme, get_filme, insert_filme, get_usuario, select_usuarios, delete_usuario, update_usuario, insert_usuario
 
 app = Flask(__name__)
-
-tabelas = ('filmes', 'generos', 'locacoes', 'pagamento', 'usuarios', 'diretores')
-
-
-# Função para retornar as tabelas existentes no atual banco de dados.
-@app.route("/", methods=["GET", "POST"])
-def hello():
-    if request.method == "GET":
-        return jsonify(query("SHOW TABLES"))
-    elif request.method == "POST":
-        return request.json
 
 
 #########################################################################
 ######################################################################### CONTROLER - GÊNEROS
-# Função para buscar dados nas tabelas.
-@app.route("/generos", methods=["GET", "POST", "DELETE", "PATCH"])
-def controller_genero():
-    if request.method == "POST":
-        genero = genero_name_web(**request.json)
-        if valida_genero(**genero):
-            insert_genero(**genero)
-            genero_criado = get_genero(genero["nome"])
-            return jsonify(genero_name_db(genero_criado))
-        else:
-            return jsonify({"Erro": "Genero inválido ou já inserido!"})
+@app.route("/generos", methods=["POST"]) #Check
+def inserir_genero():
+    genero = genero_name_web(**request.json)
+    if valida_genero(**genero):
+        id_genero = insert_genero(**genero)
+        genero_cadastrado = get_genero(id_genero)
+        return jsonify(genero_name_db(genero_cadastrado))
+    else:
+        return jsonify({"erro": "Gênero inválido ou já inserido!"})
 
-    if request.method == "GET":
-        return jsonify(get_genero(**request.json))
 
-    if request.method == "DELETE":
-        delete_genero(**request.json)
-        return jsonify(get_genero(**request.json))
+@app.route("/generos/<int:id>", methods=["PUT", "PATCH"]) #Check
+def alter_genero(id):
+    genero = genero_name_web(**request.json)
+    if valida_genero(**genero):
+        update_genero(id, **genero)
+        genero_cadastrado = get_genero(id)
+        return jsonify(genero_name_db(genero_cadastrado))
+    else:
+        return jsonify({"erro": "Usuário inválido"})
 
-    if request.method == "PATCH":
-        alter_genero(request.json['atual'], request.json['novo'])
-        return jsonify(get_genero(**request.json))
+
+@app.route("/generos/<int:id>", methods=["DELETE"]) #Not Working
+def deletar_genero(id):
+    try:
+        delete_genero(id)
+        return "", 204
+    except:
+        return jsonify({"erro": "Não foi possivel deletar"})
+
+
+@app.route("/generos", methods=["GET"])
+def buscar_genero():
+    nome = nome_genero_from_web(**request.args)
+    generos = select_genero(nome)
+    generos_name_db = [genero_name_db(genero) for genero in generos]
+    return jsonify(generos_name_db)
 
 
 #########################################################################
 ######################################################################### CONTROLLER - DIRETORES
-@app.route("/diretores", methods=["GET", "POST", "DELETE", "PATCH"])
-def controller_diretores():
-    if request.method == "GET":
-        return jsonify(get)
+@app.route("/diretores", methods=["POST"]) #Check
+def inserir_diretor():
+    diretor = diretor_name_web(**request.json)
+    if valida_diretor(**diretor):
+        id_diretor = insert_diretor(**diretor)
+        diretor_cadastrado = get_diretor(id_diretor)
+        return jsonify(diretor_name_db(diretor_cadastrado))
+    else:
+        return jsonify({"erro": "Diretor inválido ou já inserido!"})
 
 
+@app.route("/diretores/<int:id>", methods=["PUT", "PATCH"]) #Check
+def alterar_diretor(id):
+    diretor = diretor_name_web(**request.json)
+    if valida_diretor(**diretor):
+        update_diretor(id, **diretor)
+        diretor_cadastrado = get_diretor(id)
+        return jsonify(diretor_name_db(diretor_cadastrado))
+    else:
+        return jsonify({"Erro": "Usuário inválido"})
 
 
+@app.route("/diretores/<int:id>", methods=["DELETE"]) #Check
+def deletar_diretor(id):
+    try:
+        delete_diretor(id)
+        return "", 204
+    except:
+        return jsonify({"Erro": "Diretor possui itens conectados a ele"})
 
 
-
-########################################################################################## FETCH REGION
-##########################################################################################
-@app.route("/<string:rota>")
-def fecth_others(rota):
-    if rota in tabelas != "produtos":
-        return jsonify(query(f"select * from {rota} limit 10"))
-
-
-def price_str_convert(produto):
-    return (produto[0], produto[1], produto[2], str(produto[3]))
-
-@app.route("/produtos")
-def fetch_produtos():
-    produtos = [price_str_convert(produto) for produto in query("SELECT * FROM produtos")]
-    return jsonify(produtos)
-##########################################################################################
-########################################################################################## FETCH REGION END
+@app.route("/diretores", methods=["GET"]) #Check
+def buscar_diretor():
+    nome_completo = nome_diretor_from_web(**request.args)
+    diretores = select_diretor(nome_completo)
+    diretores_name_db = [diretor_name_db(diretor) for diretor in diretores]
+    return jsonify(diretores_name_db)
 
 
-
-########################################################################################## ALTER REGION
-##########################################################################################
-@app.route("/alter/<string:rota>", methods=["GET", "PATCH"])
-def alter_table(rota):
-    rota = request.json["rota"]
-    coluna = request.json["coluna"]
-    cln_dado = request.json["dado"]
-    vlr_local = request.json["valor_atual"]
-
-    if rota in tabelas:
-        modification = request.json["mod"]
-        if modification == "A":
-
-            execute(f"update {rota} set {coluna} = {cln_dado} where {coluna} = {vlr_local}")
-            return jsonify(query(f"select * from {rota} where '{coluna}' = {cln_dado}"))
-##########################################################################################
-########################################################################################## ALTER REGION END
+#########################################################################
+######################################################################### CONTROLLER - FILMES
+@app.route("/filmes", methods=["POST"]) #Check
+def inserir_filme():
+    filme = filme_name_web(**request.json)
+    if valida_filme(**filme):
+        id_filme = insert_filme(**filme)
+        filme_cadastrado = get_filme(id_filme)
+        return jsonify(filme_name_db(filme_cadastrado))
+    else:
+        return jsonify({"erro": "Filme inválido ou já cadastrado"})
 
 
-
-@app.route("/insert", methods=["GET", "POST"])
-def insert():
-    tabela = request.json["tabela"]
-    valores = request.json["valores"]
-    colunas = request.json["colunas"]
-    vlr_holder = ['%s' for vlr in valores]
-    concat_colunas = ', '.join(colunas)
-    execute(f"INSERT INTO {tabela} ({concat_colunas}) VALUES ({', '.join(vlr_holder)})", valores)
-    return jsonify(query("SELECT * FROM TB_PAIS WHERE ID = 240"))
-
-##########################################################################################
-##########################################################################################
-#Select Specific
+@app.route("/filmes/<int:id>", methods=["PUT", "PATCH"]) #Check
+def alterar_filme(id):
+    filme = filme_name_web(**request.json)
+    if valida_filme(**filme):
+        update_filme(id, **filme)
+        filme_cadastrado = get_filme(id)
+        return jsonify(filme_name_db(filme_cadastrado))
+    else:
+        return jsonify({"Erro": "Filme inválido ou não existente!"})
 
 
+@app.route("/filmes/<int:id>", methods=["DELETE"]) #Check
+def deletar_filme(id):
+    try:
+        delete_fime(id)
+        return "", 204
+    except:
+        return jsonify({"Erro": "Filme possui itens conectados a ele"})
+
+
+@app.route("/filmes", methods=["GET"]) #Check
+def buscar_filme():
+    titulo = titulo_filme_from_web(**request.args)
+    filmes = select_filme(titulo)
+    filmes_name_db = [filme_name_db(filme) for filme in filmes]
+    return jsonify(filmes_name_db)
+
+
+#########################################################################
+######################################################################### CONTROLLER - Usuários
+@app.route("/usuarios", methods=["POST"])
+def inserir_usuario():
+    usuario = usuario_from_web(**request.json)  # 3 - formata o que vem da internet
+    if valida_usuario(**usuario):
+        id_usuario = insert_usuario(**usuario)
+        usuario_cadastrado = get_usuario(id_usuario)
+        return jsonify(usuario_from_db(usuario_cadastrado))
+    else:
+        return jsonify({"erro": "Usuário inválido"})
+
+
+@app.route("/usuarios/<int:id>", methods=["PUT", "PATCH"])
+def alterar_usuario(id):
+    usuario = usuario_from_web(**request.json)  # 3 - formata o que vem da internet
+    if valida_usuario(**usuario):
+        update_usuario(id, **usuario)
+        usuario_cadastrado = get_usuario(id)
+        return jsonify(usuario_from_db(usuario_cadastrado))
+    else:
+        return jsonify({"erro": "Usuário inválido"})
+
+
+@app.route("/usuarios/<int:id>", methods=["DELETE"])
+def apagar_usuario(id):
+    try:
+        delete_usuario(id)
+        return "", 204
+    except:
+        return jsonify({"erro": "Usuário possui itens conectados a ele"})
+
+
+@app.route("/usuarios", methods=["GET"])
+def buscar_usuario():
+    nome_completo = nome_usuario_from_web(**request.args)
+    usuarios = select_usuarios(nome_completo)
+    usuarios_from_db = [usuario_from_db(usuario) for usuario in usuarios]
+    return jsonify(usuarios_from_db)
 
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", debug=True)
-
-# jsonify({"metodo": request.method})
-
--- models.py
+++ models.py
@@ -1,37 +1,90 @@
-from main import insert, delete, update, query
-
+from main import insert, delete, update, query, select, select_like
 
 
 
 ############################################################################## Funções - Gênero
 ##############################################################################
-
-list_holder = query("SELECT * FROM generos")
-
-
 def insert_genero(nome):
-    insert('generos', ('nome',), (nome,))
+    return insert('generos', ('nome',), (nome,))
 
 
-def alter_genero(atual, novo):
-    update('generos', 'nome', atual, 'nome', novo)
+def update_genero(id_atual, nome):
+    update('generos', 'id', id_atual, ('nome',), [nome,])
 
 
-def get_genero(nome):
-    return query("SELECT * FROM generos")
+def get_genero(id_genero):
+    return select("generos", "id", id_genero)[0]
 
 
-def delete_genero(nome):
-    delete("generos", "nome", nome)
-    query("SELECT * FROM generos")
+def select_genero(nome):
+    return select_like("generos", "nome", nome)
+
+
+def delete_genero(id_genero):
+    delete("generos", "id", id_genero)
 
 
 ##############################################################################
 ############################################################################## Funções - Diretor
+def get_diretor(id_diretor):
+    return select("diretores", "id", id_diretor)[0]
 
-def get_diretor(nome):
-    return query("SELECT * FROM diretores")
+
+def insert_diretor(nome_completo):
+    return insert('diretores', ('nome_completo',), (nome_completo,))
 
 
-def insert_diretor(nome):
-    insert('diretores', 'nome_completo', nome)
+def select_diretor(nome_completo):
+    return select_like("diretores", "nome_completo", nome_completo)
+
+
+def delete_diretor(id_diretor):
+    delete("diretores", "id", id_diretor)
+
+
+def update_diretor(id_diretor, nome_completo):
+    update('diretores', 'id', id_diretor, ('nome_completo',), [nome_completo,])
+
+
+##############################################################################
+############################################################################## Funções - FILMES
+def get_filme(id_filme):
+    return select("filmes", "id", id_filme)[0]
+
+
+def insert_filme(titulo, ano, classificacao, preco, diretores_id, generos_id):
+    return insert('filmes', ('titulo', 'ano', 'classificacao', 'preco', 'diretores_id', 'generos_id'), (titulo, ano, classificacao, preco, diretores_id, generos_id))
+
+
+def update_filme(id_filme, titulo, ano, classificacao, preco, diretores_id, generos_id):
+    update("filmes", "id", id_filme, ["titulo", "ano", "classificacao", "preco", "diretores_id", "generos_id"], [titulo, ano, classificacao, preco, diretores_id, generos_id])
+
+
+def delete_fime(id_filme):
+    delete("filmes", "id", id_filme)
+
+
+def select_filme(titulo):
+    return select_like("filmes", "titulo", titulo)
+
+
+##############################################################################
+############################################################################## Funções - FILMES
+def insert_usuario(nome_completo, CPF):
+    return insert("usuarios", ["nome_completo", "CPF"], [nome_completo, CPF])
+
+
+def get_usuario(id_usuario):
+    return select("usuarios", "id", id_usuario)[0]
+
+
+def update_usuario(id_usuario, nome_completo, CPF):
+    update("usuarios", "id", id_usuario, ["nome_completo", "CPF"], [nome_completo, CPF])
+
+
+def delete_usuario(id_usuario):
+    delete("usuarios", "id", id_usuario)
+
+
+def select_usuarios(nome_completo):
+    return select_like("usuarios", "nome_completo", nome_completo)
